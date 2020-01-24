04c4ef34537b5ee03be3b8eb68b4397b94aacd1b
Jason DeVito <jdevito@pepperdash.com>
2020-01-24 14:49:28 -0600

    TE-1-tesira-plugin-bugs updates

--- Tesira-DSP-EPI/TesiraDspControlPoint.cs
+++ Tesira-DSP-EPI/TesiraDspControlPoint.cs
@@ -3,164 +3,197 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Crestron.SimplSharp;
+using PepperDash.Core;
 using PepperDash.Essentials.Devices.Common.DSP;
 
-namespace Tesira_DSP_EPI {
-    public class TesiraDspControlPoint {
-        public string Key { get; protected set; }
+namespace Tesira_DSP_EPI
+{
+	public class TesiraDspControlPoint : IKeyed
+	{
+		public string Key { get; protected set; }
+		public string InstanceTag1 { get; set; }
+		public string InstanceTag2 { get; set; }
+		public int Index1 { get; set; }
+		public int Index2 { get; set; }
+		public TesiraDsp Parent { get; private set; }
+		public bool Enabled { get; set; }
+		public string Label { get; set; }
 
-        public string InstanceTag1 { get; set; }
-        public string InstanceTag2 { get; set; }
-        public int Index1 { get; set; }
-        public int Index2 { get; set; }
-        public TesiraDsp Parent { get; private set; }
-        public bool Enabled { get; set; }
-        public string Label { get; set; }
+		public virtual bool IsSubscribed { get; protected set; }
 
-        public virtual bool IsSubscribed { get; protected set; }
+		protected TesiraDspControlPoint(string instanceTag1, string instanceTag2, int index1, int index2, TesiraDsp parent)
+		{
+			InstanceTag1 = string.IsNullOrEmpty(instanceTag1) ? "" : instanceTag1;
+			InstanceTag2 = string.IsNullOrEmpty(instanceTag2) ? "" : instanceTag2;
+			Index1 = index1;
+			Index2 = index2;
+			Parent = parent;
+		}
 
-        protected TesiraDspControlPoint(string instanceTag1, string instanceTag2, int index1, int index2, TesiraDsp parent) {
-            InstanceTag1 = instanceTag1;
-            InstanceTag2 = instanceTag2;
-            Index1 = index1;
-            Index2 = index2;
-            Parent = parent;
-        }
+		virtual public void Initialize()
+		{
 
-        virtual public void Initialize() {
+		}
 
-        }
+		virtual public void Subscribe()
+		{
 
-        virtual public void Subscribe() {
+		}
 
-        }
+		/// <summary>
+		/// Sends a command to the DSP
+		/// </summary>
+		/// <param name="command">command</param>
+		/// <param name="attribute">attribute code</param>
+		/// <param name="value">value (use "" if not applicable)</param>
+		public virtual void SendFullCommand(string command, string attributeCode, string value, int InstanceTag)
+		{
+			if (string.IsNullOrEmpty(attributeCode))
+			{
+				Debug.Console(2, this, Debug.ErrorLogLevel.Error, "SendFullCommand({0}, {1}, {2}, {3}) Error: AttributeCode is null or empty", command, attributeCode, value, InstanceTag);
+				return;
+			}
 
-        /// <summary>
-        /// Sends a command to the DSP
-        /// </summary>
-        /// <param name="command">command</param>
-        /// <param name="attribute">attribute code</param>
-        /// <param name="value">value (use "" if not applicable)</param>
-        public virtual void SendFullCommand(string command, string attributeCode, string value, int InstanceTag) {
-            // Command Format: InstanceTag get/set/toggle/increment/decrement/subscribe/unsubscribe attributeCode [index] [value]
-            // Ex: "RoomLevel set level 1.00"
-            string instanceTag;
-            switch (InstanceTag) {
-                case 1: 
-                    instanceTag = InstanceTag1;
-                    break;
-                case 2:
-                    instanceTag = InstanceTag2;
-                    break;
-                case 999:
-                    instanceTag = "DEVICE";
-                    break;
-                default:
-                    instanceTag = InstanceTag1;
-                    break;
-            }
-            string cmd;
+			// Command Format: InstanceTag get/set/toggle/increment/decrement/subscribe/unsubscribe attributeCode [index] [value]
+			// Ex: "RoomLevel set level 1.00"
+			string cmd;
+			string instanceTag;
+			switch (InstanceTag)
+			{
+				case 1:
+					instanceTag = InstanceTag1;
+					break;
+				case 2:
+					instanceTag = InstanceTag2;
+					break;
+				case 999:
+					instanceTag = "DEVICE";
+					break;
+				default:
+					instanceTag = InstanceTag1;
+					break;
+			}
 
-            if (attributeCode == "level" || attributeCode == "mute" || attributeCode == "minLevel" || 
-                attributeCode == "maxLevel" || attributeCode == "label" || attributeCode == "rampInterval" || 
-                attributeCode == "rampStep" || attributeCode == "autoAnswer" || attributeCode == "dndEnable" ||
-                attributeCode == "dtmf" ||  attributeCode == "state") {
-                //Command requires Index
-
-                if (String.IsNullOrEmpty(value)) {
-                    if(String.IsNullOrEmpty(command)) {
-                        //format command without valur OR command
-                        cmd = string.Format("{0} {1} {2} ", instanceTag, attributeCode, Index1);
-                    }
-                    else {
-                        // format command without value
-                        cmd = string.Format("{0} {1} {2} {3}", instanceTag, command, attributeCode, Index1);
-                    }
-                
-                }
-
-                else {
-                    // format commadn with value
-                    cmd = string.Format("{0} {1} {2} {3} {4}", instanceTag, command, attributeCode, Index1, value);
-                }
-
-            }
+			if (attributeCode == "level" || attributeCode == "mute" || attributeCode == "minLevel" ||
+				attributeCode == "maxLevel" || attributeCode == "label" || attributeCode == "rampInterval" ||
+				attributeCode == "rampStep" || attributeCode == "autoAnswer" || attributeCode == "dndEnable" ||
+				attributeCode == "dtmf" || attributeCode == "state")
+			{
+				//Command requires Index
+				if (String.IsNullOrEmpty(value))
+				{
+					if (String.IsNullOrEmpty(command))
+					{
+						//format command without value OR command
+						cmd = string.Format("{0} {1} {2} ", instanceTag, attributeCode, Index1);
+					}
+					else
+					{
+						// format command without value
+						cmd = string.Format("{0} {1} {2} {3}", instanceTag, command, attributeCode, Index1);
+					}
+				}
+				else
+				{
+					// format commadn with value
+					cmd = string.Format("{0} {1} {2} {3} {4}", instanceTag, command, attributeCode, Index1, value);
+				}
+			}
 
 
-            else if (attributeCode == "dial" || attributeCode == "end" || attributeCode == "onHook" || 
-                attributeCode == "offHook" || attributeCode == "answer") {
-                //requires index, but does not require command
+			else if (attributeCode == "dial" || attributeCode == "end" || attributeCode == "onHook" ||
+				attributeCode == "offHook" || attributeCode == "answer")
+			{
+				//requires index, but does not require command
+				if (String.IsNullOrEmpty(value))
+				{
+					//format command without value
+					cmd = string.Format("{0} {1} {2} {3}", instanceTag, attributeCode, Index1, Index2);
+				}
+				else
+				{
+					cmd = string.Format("{0} {1} {2} {3} {4}", instanceTag, attributeCode, Index1, Index2, value);
+				}
+			}
 
-                if (String.IsNullOrEmpty(value)) {
-                    //format command without value
-                    cmd = string.Format("{0} {1} {2} {3}", instanceTag, attributeCode, Index1, Index2);
-                }
+			else
+			{
+				//Command does not require Index
+				if (String.IsNullOrEmpty(value))
+				{
+					cmd = string.Format("{0} {1} {2}", instanceTag, command, attributeCode);
+				}
+				else
+				{
+					cmd = string.Format("{0} {1} {2} {3}", instanceTag, command, attributeCode, value);
+				}
+			}
 
-                cmd = string.Format("{0} {1} {2} {3} {4}", instanceTag, attributeCode, Index1, Index2, value);
-            }
+			if (command == "get")
+			{
+				// This command will generate a return value response so it needs to be queued
+				if(!string.IsNullOrEmpty(cmd))
+					Parent.EnqueueCommand(new TesiraDsp.QueuedCommand { Command = cmd, AttributeCode = attributeCode, ControlPoint = this });
+			}
+			else
+			{
+				// This command will generate a simple "+OK" response and doesn't need to be queued
+				if(!string.IsNullOrEmpty(cmd))
+					Parent.SendLine(cmd);
+			}
+		}
 
-            else {
-                //Command does not require Index
+		virtual public void ParseGetMessage(string attributeCode, string message)
+		{
 
-                if (String.IsNullOrEmpty(value)) {
-                    cmd = string.Format("{0} {1} {2}", instanceTag, command, attributeCode);   
-                }
-                else {                   
-                    cmd = string.Format("{0} {1} {2} {3}", instanceTag, command, attributeCode, value);
-                }
-            }
+		}
 
-            if (command == "get") {
-                // This command will generate a return value response so it needs to be queued
-                Parent.EnqueueCommand(new TesiraDsp.QueuedCommand { Command = cmd, AttributeCode = attributeCode, ControlPoint = this });
-            }
-            else {
-                // This command will generate a simple "+OK" response and doesn't need to be queued
-                Parent.SendLine(cmd);
-            }
+		public virtual void SendSubscriptionCommand(string customName, string attributeCode, int responseRate, int InstanceTag)
+		{
+			// Subscription string format: InstanceTag subscribe attributeCode Index1 customName responseRate
+			// Ex: "RoomLevel subscribe level 1 MyRoomLevel 500"
+			if (string.IsNullOrEmpty(customName) || string.IsNullOrEmpty(attributeCode))
+			{
+				Debug.Console(2, this, "SendSubscriptionCommand({0}, {1}, {2}, {3}) Error: CustomName or AttributeCode are null or empty", customName, attributeCode, responseRate, InstanceTag);
+				return;
+			}
 
-        }
+			string cmd;
+			string instanceTag;
+			switch (InstanceTag)
+			{
+				case 1:
+					instanceTag = InstanceTag1;
+					break;
+				case 2:
+					instanceTag = InstanceTag2;
+					break;
 
-        virtual public void ParseGetMessage(string attributeCode, string message) {
+				default:
+					instanceTag = InstanceTag1;
+					break;
+			}
+			if (attributeCode == "callState" || attributeCode == "sourceSelection")
+			{
+				cmd = string.Format("\"{0}\" subscribe {1} {2} {3}", instanceTag, attributeCode, customName, responseRate);
+			}
 
-        }
+			else if (responseRate > 0)
+			{
+				cmd = string.Format("\"{0}\" subscribe {1} {2} {3} {4}", instanceTag, attributeCode, Index1, customName, responseRate);
+			}
+			else
+			{
+				cmd = string.Format("\"{0}\" subscribe {1} {2} {3}", instanceTag, attributeCode, Index1, customName);
+			}
 
-        public virtual void SendSubscriptionCommand(string customName, string attributeCode, int responseRate, int InstanceTag) {
-            // Subscription string format: InstanceTag subscribe attributeCode Index1 customName responseRate
-            // Ex: "RoomLevel subscribe level 1 MyRoomLevel 500"
+			//Parent.WatchDogList.Add(customName,cmd);
+			Parent.SendLine(cmd);
+		}
 
-            string cmd;
+		public virtual void DoPoll()
+		{
 
-            string instanceTag;
-            switch (InstanceTag) {
-                case 1:
-                    instanceTag = InstanceTag1;
-                    break;
-                case 2:
-                    instanceTag = InstanceTag2;
-                    break;
-
-                default:
-                    instanceTag = InstanceTag1;
-                    break;
-            }
-            if (attributeCode == "callState" || attributeCode == "sourceSelection") {
-                cmd = string.Format("\"{0}\" subscribe {1} {2} {3}", instanceTag, attributeCode, customName, responseRate);
-            }
-
-            else if (responseRate > 0) {
-                cmd = string.Format("\"{0}\" subscribe {1} {2} {3} {4}", instanceTag, attributeCode, Index1, customName, responseRate);
-            }
-            else {
-                cmd = string.Format("\"{0}\" subscribe {1} {2} {3}", instanceTag, attributeCode, Index1, customName);
-            }
-
-            //Parent.WatchDogList.Add(customName,cmd);
-            Parent.SendLine(cmd);
-        }
-
-        public virtual void DoPoll() {
-
-        }
-    }
+		}
+	}
 }
